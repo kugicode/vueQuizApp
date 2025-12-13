dc1e130033f664ccd3164f3cc0da0d25d8a7bcba
Anas Hersi <101137632+kugicode@users.noreply.github.com>
2025-12-13 23:59:13 +0000

    Started the project, making a quiz app with vue js to practive and learn.

---
+++ src/Components/Quiz.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+
+const score = ref(0);
+const currentQuestionIndex = ref(0);
+
+
+const questions = [
+    {
+        question: "What is 2+2!",
+        options: ["2", "4", "1", "5"],
+        answer: "2" 
+    },
+
+    {
+        question: "What language is Vue.js built with?",
+        options: ["Python", "Java", "Javascript", "None"],
+        answer: "Javascript"
+    }
+]
+
+</script>
+
+<template>
+
+</template>
